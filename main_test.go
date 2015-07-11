d583d658d6da0b2f95ab3bcd27cd7d4bd93c3fc0
Xabier Larrakoetxea <slok69@gmail.com>
2015-07-11 13:00:48 +0200

    Add main function server up and running test
    Signed-off-by: Xabier Larrakoetxea <slok69@gmail.com>

--- main_test.go
+++ main_test.go
@@ -1,18 +1,41 @@
-package main_test
+package main
 
 import (
+	"fmt"
+	"net/http"
 	"testing"
+	"time"
 
+	"github.com/spf13/viper"
 	. "gopkg.in/check.v1"
 )
 
 // Hook up gocheck into the "go test" runner.
 func TestMain(t *testing.T) { TestingT(t) }
 
-type MainSuite struct{}
+type MainSuite struct {
+	url string
+}
 
 var _ = Suite(&MainSuite{})
 
-func (s *MainSuite) TestHelloWorld(c *C) {
-	c.Assert(42, Equals, 42)
+func (s *MainSuite) SetUpSuite(c *C) {
+	// Overwrite settings for the tests
+	viper.Set("Port", 7357)
+	s.url = fmt.Sprintf("http://127.0.0.1:%d", viper.GetInt("Port"))
+}
+
+func (s *MainSuite) TestServerStart(c *C) {
+	// Call the server
+	go main()
+
+	// Wait to start
+	time.Sleep(time.Second)
+
+	// Check server is up
+	_, err := http.Get(s.url)
+
+	if err != nil {
+		c.Errorf("Server should be responding...: %v", err)
+	}
 }
